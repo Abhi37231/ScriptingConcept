44ad03cfc7397f3ba8a544fd8c5d2b9d57a1909a
Abhi37231 <159634392+Abhi37231@users.noreply.github.com>
2026-02-16 15:12:27 +0530

    16/02

---
+++ TypeScript/var.ts
@@ -0,0 +1,22 @@
+// 1. String
+let firstName: string = "Abhinandan";
+
+// 2. Number
+let score: number = 95;
+let price: number = 19.99;
+
+// 3. Boolean
+let isStudent: boolean = true;
+
+// 4. Array
+let colors: string[] = ["Red", "Green", "Blue"];
+
+// 5. Const 
+const gravity: number = 9.8;
+
+console.log("--- My Variables ---");
+console.log(`Name: ${firstName}`);
+console.log(`Score: ${score}`);
+console.log(`Is a student? ${isStudent}`);
+console.log(`First color in list: ${colors[0]}`);
+console.log(`Gravity constant: ${gravity}`);
